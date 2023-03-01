d5050ab394fea1c768c71fccb74f0fb04e2ac48e
omerenma <godwin2341@gmail.com>
2023-03-01 16:33:47 +0100

    work on navbar responsiveness

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -12,7 +12,7 @@ const Features = () => {
         <div className='u-title'>
           <BsFillBookFill color="orange" size={30} />
           <h2>Just a little about me</h2>
-          <p className='u-text-small u-text-light'>
+          <p className='u-text-small u-text-light responsive-style'>
           Hi, I'm Kingsley, a Full Stack software Engineer who is passionate about building solutions that scales across multiple platforms.
         I recently developed a full stack B2B and B2C e-commerce application using Node.js with Typescript and Angular that connects  businesses to customers with proximity functionality to show users and customers businesses with products of interest close to them.
       I work with JavaScript | Typescript | React | React-Native | Angular
