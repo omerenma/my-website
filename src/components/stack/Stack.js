d66b0b8d76051edc19519fa59652387c378d02a0
omerenma <godwin2341@gmail.com>
2023-03-02 11:45:41 +0100

    work on tech stack responsiveness

--- src/components/stack/Stack.js
+++ src/components/stack/Stack.js
@@ -10,10 +10,9 @@ const Stack = () => {
     <div className='container stack'>
       <div className='u-title'>
       <BsFillBriefcaseFill size={30} color="orange" />
-      <h2>Stack</h2>
+      <h2>Technologies I have worked with</h2>
           <p className='u-text-small u-text-light'>
-         I have worked extensively with JavaScript and TypeScript and other JavaScript framework
-         to build stunning and amazing applications
+         I have worked professionally and extensively with the following technologies
           </p>
         </div>
           <div className='description-div'>
