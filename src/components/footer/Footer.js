7c3f7aa37f95822d4677c78a238bd3940e43c66f
omerenma <godwin2341@gmail.com>
2023-03-02 16:45:13 +0100

    footer responsiveness

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Footer.css'
+import {FaPhoneAlt, FaEnvelope, FaGlobe, FaMap} from 'react-icons/fa'
+import {BiNavigation} from 'react-icons/bi'
+import {FcContacts} from 'react-icons/fc'
+
 const Footer = () => {
   return (
     <footer id='footer'>
       <div className='footer container'>
         <div className='footer-box'>
-        <h2>Links</h2>
+        <h2><BiNavigation color='orange' /></h2>
           <div className='footer-link'>
             <a href='/'>Home</a>
             <a href='#about'>About</a>
@@ -14,7 +18,7 @@ const Footer = () => {
           </div>
         </div>
         <div className='footer-box'>
-        <h2>Social</h2>
+        <h2><FaGlobe color='orange' /></h2>
           <div className='footer-link'>
             <a href='/'>Github</a>
             <a href='#about'>Linkedin</a>
@@ -22,10 +26,10 @@ const Footer = () => {
           </div>
         </div>
         <div className='footer-box'>
-        <h2>Contact</h2>
+        <h2><FcContacts color='orange' /></h2>
           <div className='footer-link'>
-            <a href='/'><span>Phone:</span>+2348130299583</a>
-            <a href='#about'><span>Email:</span>Godwin2341@gmail.com</a>
+            <a href='/'><span><FaPhoneAlt color='orange' /></span>+2348130299583</a>
+            <a href='#about'><span><FaEnvelope color='orange' /></span>Godwin2341@gmail.com</a>
           </div>
         </div>
 
