aa30bf5a990db289702b2b74d57f55c6cc96b522
omerenma <godwin2341@gmail.com>
2023-03-02 15:46:08 +0100

    added footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,36 @@
 import React from 'react'
-import {} from './Footer.css'
+import './Footer.css'
 const Footer = () => {
   return (
-    <section id='footer'>
-      <h1>Footer</h1>
-    </section>
+    <footer id='footer'>
+      <div className='footer container'>
+        <div className='footer-box'>
+        <h2>Links</h2>
+          <div className='footer-link'>
+            <a href='/'>Home</a>
+            <a href='#about'>About</a>
+            <a href='#projects'>Project</a>
+            <a href='#social'>Social</a>
+          </div>
+        </div>
+        <div className='footer-box'>
+        <h2>Social</h2>
+          <div className='footer-link'>
+            <a href='/'>Github</a>
+            <a href='#about'>Linkedin</a>
+            <a href='#social'>Social</a>
+          </div>
+        </div>
+        <div className='footer-box'>
+        <h2>Contact</h2>
+          <div className='footer-link'>
+            <a href='/'><span>Phone:</span>+2348130299583</a>
+            <a href='#about'><span>Email:</span>Godwin2341@gmail.com</a>
+          </div>
+        </div>
+
+      </div>
+    </footer>
   )
 }
 
