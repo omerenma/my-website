7c5d1417cdd48e06aca4e68f370645b2b8d0aa02
omerenma <godwin2341@gmail.com>
2023-03-03 10:28:11 +0100

    added scroll to top

--- src/components/ui/button/ScrollToTopButton.js
+++ src/components/ui/button/ScrollToTopButton.js
@@ -1,27 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {BsArrowUpCircle} from 'react-icons/bs'
 
 function ScrollToTopButton() {
   const [isVisible, setIsVisible] = useState(false);
 
-  const handleScroll = () => {
-    const scrollTop = window.pageYOffset;
-    setIsVisible(scrollTop > 0);
-  };
+//   const handleScroll = () => {
+//     const scrollTop = window.pageYOffset;
+//     setIsVisible(scrollTop > 0);
+//   };
 
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
+//   const scrollToTop = () => {
+//     window.scrollTo({ top: 0, behavior: "smooth" });
+//   };
 
-  React.useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+//   React.useEffect(() => {
+//     window.addEventListener("scroll", handleScroll);
+//     return () => window.removeEventListener("scroll", handleScroll);
+//   }, []);
 
+useEffect(() => {
+    const handleScrollButtonVisibility = () => {
+        window.pageYOffset > 300 ? setIsVisible(true) : setIsVisible(false);
+    }
+        window.addEventListener('scroll', handleScrollButtonVisibility);
+        return () => {
+            window.removeEventListener('scroll', handleScrollButtonVisibility)
+        }
+    
+}, [])
+const handleScrollToTop = () => {
+    window.scrollTo({top:0, behavior:"smooth"})
+}
   return (
     <>
       {isVisible && (
-        <button className="scroll-to-top-btn" style={{border:'none', position:'absolute', right:0}} onClick={scrollToTop}>
+        <button className="scroll-to-top-btn" style={{border:'none', background:"transparent", position:'fixed', right:0,bottom:10}} onClick={handleScrollToTop}>
          <BsArrowUpCircle color="orange" size={30}/>
         </button>
       )}
